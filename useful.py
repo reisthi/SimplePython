1608c257590f7cd43b6e3923aa41e59c12e0caae
reisthi <thiagoth.reis@gmail.com>
2018-07-08 21:30:54 -0500

    added 'delete_duplicates'

---
+++ useful.py
@@ -0,0 +1,14 @@
+"""
+This file has functions that can be used on the daily basis.
+"""
+
+def delete_duplicates(model_name):
+    """This function deletes duplicates from a model on the database.
+        It takes a ModelName as argument.
+    """
+    deleted = 0
+    for row in model_name.objects.all():
+        if model_name.objects.filter(isbn_13=row.isbn_13).count() > 1:
+            model_name.delete()
+            deleted += 1
+    print(f"deleted: {deleted}")
