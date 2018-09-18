81c9b06de412e770ec162338e80494096b7fc23e
reisthi <thiagoth.reis@gmail.com>
2018-09-18 09:04:21 -0500

    tricks

--- tricks.py
+++ tricks.py
@@ -1,6 +1,4 @@
-"""
-Add comma to the end of a list to make it easier for code review
-"""
+""" Add comma to the end of a list to make it easier for code review. """
 
 # This is a bad example:
 a_list = ['one', 'two', 'three']
